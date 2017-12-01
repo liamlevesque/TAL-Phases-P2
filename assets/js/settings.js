fb4747c0517240bf6f297d7ca0d104745eee807f
liamlevesque <liamlevesque@gmail.com>
2017-11-30 22:13:34 -0800

    added time extensions admin

---
+++ assets/js/settings.js
@@ -0,0 +1,51 @@
+const settings = {
+
+    validTimes: [ 30, 60, 90, 120, 150, 180, 210, 240],
+    
+    extensionsOn: true,
+    firstExtension: {
+        threshold: 120,
+        extendMethod: 'By',
+        extension: 60, 
+    },
+    secondExtension: {
+        enabled: true,
+        threshold: 60,
+        extendMethod: 'To',
+        extension: 120, 
+    },
+    extensionsLimit:{
+        enabled: false,
+        limit: 10,
+        min: 0,
+        max: 100,
+        steps: 5
+    }
+    
+};
+
+Vue.use(vueDirectiveTooltip, {
+    delay: 500,
+    placement: 'top',
+    class: 'tooltip-rb',
+    triggers: ['hover','click'],
+    offset: 0
+});
+
+var app = new Vue({
+    el: ".js--tal",
+    data: settings,
+    methods: {
+
+
+    },
+    filters:{
+        secondsToTime: function(value){
+            if (value) {
+                let minutes = Math.floor(value/60) + 'm ';
+                let seconds = (value % 60 != 0) ? (value % 60) + 's' : '';
+                return minutes + seconds;
+            }
+        }
+    }
+});
